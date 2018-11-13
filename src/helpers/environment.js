e0da35a370ad7836182e548d21213529970e3215
Emily Perkins <eaperkins34@gmail.com>
2018-11-13 11:11:05 -0500

    update

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -5,7 +5,7 @@ switch (window.location.hostname) {
         APIURL = 'http://localhost:3000';
         break;
     case 'cuddleclient.herokuapp.com':
-        APIURL = 'https://cuddleserver.herokuapp.com/'
+        APIURL = 'https://cuddleserver.herokuapp.com'
 }
 
 export default APIURL;  
